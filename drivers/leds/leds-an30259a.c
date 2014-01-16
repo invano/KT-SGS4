00fc6a9b1a579e92afadc36a85d8613e4b5e43b9
invano <cozziemanuele@gmail.com>
2014-01-16 20:46:45 +0100

    Added possibility to restrict current as Samsung does. Some changes.

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -74,7 +74,7 @@
 #define LED_IMAX_SHIFT                        6
 #define AN30259A_CTN_RW_FLG                0x80
 
-#define LED_R_CURRENT                0x28	// 40 decimal 
+#define LED_R_CURRENT                0x28
 #define LED_G_CURRENT                0x28
 #define LED_B_CURRENT                0x28
 #define LED_MAX_CURRENT                0xFF
@@ -344,26 +344,12 @@ static void an30259a_start_led_pattern(int mode)
                 LED_DYNAMIC_CURRENT = 0x8;
         else
                 LED_DYNAMIC_CURRENT = 0x1;
-<<<<<<< HEAD
                 
                 
 	led_r_brightness = led_intensity / LED_DYNAMIC_CURRENT;
 	led_g_brightness = led_intensity / LED_DYNAMIC_CURRENT;
 	led_b_brightness = led_intensity / LED_DYNAMIC_CURRENT;
 	
-=======
-
-	if (led_intensity == 0) {	// then use stock values
-		led_r_brightness = LED_R_CURRENT;
-		led_g_brightness = LED_G_CURRENT;
-		led_b_brightness = LED_B_CURRENT;
-	}
-	else {	// otherwise brightness adapts to led_intensity
-		led_r_brightness = led_intensity / LED_DYNAMIC_CURRENT;
-		led_g_brightness = led_intensity / LED_DYNAMIC_CURRENT;
-		led_b_brightness = led_intensity / LED_DYNAMIC_CURRENT;
-	}
->>>>>>> 8bcaff5e8a2c8d0c1b6b0e2aab9c2c2d9a421322
 	
         switch (mode) {
         /* leds_set_slope_mode(client, LED_SEL, DELAY,  MAX, MID, MIN,
@@ -399,7 +385,7 @@ static void an30259a_start_led_pattern(int mode)
  		leds_on(LED_G, true, false, led_g_brightness);
                 break;
 
-        case POWERING:	// never use LOW_POWER during boot
+        case POWERING:
                 pr_info("LED Powering Pattern on\n");
  		leds_on(LED_G, true, true, LED_G_CURRENT);
  		leds_on(LED_B, true, true, LED_B_CURRENT);
@@ -442,14 +428,7 @@ static void an30259a_set_led_blink(enum an30259a_led_enum led,
                 LED_DYNAMIC_CURRENT = LED_B_CURRENT;
 
         /* In user case, LED current is restricted */
-<<<<<<< HEAD
         if (led_intensity_restrict == 1)
-=======
-        if (led_intensity == 40) {	// if stock intesity is used (see LED_x_CURRENT = 0x28)
-        	brightness = (brightness * LED_DYNAMIC_CURRENT) / LED_MAX_CURRENT;
-        }
-        else if (led_intensity != 0) {	// adapt current to led_intensity
->>>>>>> 8bcaff5e8a2c8d0c1b6b0e2aab9c2c2d9a421322
         	brightness = (brightness * led_intensity) / LED_MAX_CURRENT;
 
         if (delay_on_time > SLPTT_MAX_VALUE)
